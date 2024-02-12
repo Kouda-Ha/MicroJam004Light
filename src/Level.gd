34c462c67e78a7303578afe2ea315988d1823416
Kouda <kdark@asia.com>
2024-02-12 11:51:25 +0000

    Making menu
    - Not much time left until the game jam submission ends so making the last
    bits now
    - Made a fader for fading in and out of a scene
    - Made a menu folder and some menu scenes, a small set up for menu to overlay
    and look pretty
    - added Assets folder and sound and font folder, will add a font shortly

--- src/Level.gd
+++ src/Level.gd
@@ -1,17 +1,17 @@
 extends Spatial
 
 onready var player = $GridMap/Player
-onready var collectable_cube = $GridMap/BorgContainer
+onready var orb_container = $GridMap/OrbContainer
 
-var borg_points_got = 0
-var total_borg_count = 0
+var collected_orbs = 0
+var total_orb_count = 0
 
 func _ready():
-	total_borg_count = collectable_cube.get_child_count()
+	total_orb_count = orb_container.get_child_count()
 	
-	player.connect("join_borg", self, "on_borg_join")
+	player.connect("orb_collected", self, "on_orb_collected")
 	
-func on_borg_join():
-	borg_points_got += 1
-	if total_borg_count >= total_borg_count:
+func on_orb_collected():
+	collected_orbs += 1
+	if collected_orbs >= total_orb_count:
 		print("Resistance is futile! Welcome Borg fren!!!!!")
